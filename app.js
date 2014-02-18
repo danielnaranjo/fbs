3a3c6ab027221bfb17e0b84838676810af74e83e
Daniel Naranjo <dnaranjo@gmail.com>
2014-02-18 18:15:17 -0430

    New Relic

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
 // Start sails and pass it command line arguments
-var newrelic = require('newrelic');
-require('sails').lift(require('optimist').argv);
+require('newrelic').require('sails').lift(require('optimist').argv);
