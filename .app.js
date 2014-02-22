e576b36cd56ed4513ac3abc7969eb5f09a8577f1
Daniel Naranjo <dnaranjo@gmail.com>
2014-02-22 11:39:30 -0430

    Added New Relic

--- .app.js
+++ .app.js
@@ -1,2 +1,16 @@
 // Start sails and pass it command line arguments 
-require('sails').lift();
+//require('sails').lift();
+
+
+// Take from:
+// https://github.com/sgress454/angular-on-sails/blob/master/app.js
+var sails;
+try {
+	sails = require('sails');
+}
+catch (e) {
+	console.error('Error: Somethings is wrong..');
+	return;
+}
+// Start server
+sails.lift();
