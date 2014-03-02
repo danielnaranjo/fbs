805cf1940735b8e79f2e8791e6587eeadc1535ac
Daniel Naranjo <dnaranjo@gmail.com>
2014-03-02 13:26:41 -0430

    Clean up

--- assets/linker/js/init.js
+++ assets/linker/js/init.js
@@ -6,29 +6,6 @@
 		var lng = position.coords.longitude;
 		miubicacion[0]=lat.toFixed(3);
 		miubicacion[1]=lng.toFixed(3);
-		//console.log(miubicacion[0]+','+miubicacion[1]);
-	}
-	function onError() {
-		if (navigator.geolocation) {
-			$('#dondeestoy').append("Error: The Geolocation service failed.");
-			$('#dondeestoy').css('display', 'inline');
-			$("#dondeestoy").removeClass().addClass("fadeInUp");
-			setTimeout(function() {
-				$('#dondeestoy').removeClass().addClass("fadeOutUp");
-			}, 5000);
-		} else {
-			$('#dondeestoy').append("Error: Your browser doesn't support geolocation. Are you in Siberia?");
-			$('#dondeestoy').css('display', 'inline');
-			$("#dondeestoy").removeClass().addClass("fadeInUp");
-			setTimeout(function() { $('#dondeestoy').removeClass().addClass("fadeOutUp");}, 5000);
-		}
-	}
-//
-
-$(document).ready(function(e) {
-//	
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition, onError);
 		setTimeout(function() {
 			$('#dondeestoy').html('');
 			$('#dondeestoy').css('display', 'inline');
@@ -42,26 +19,74 @@ $(document).ready(function(e) {
 			// Mostrar datos en la caja de busqueda
 			//$("#w").val('Estas cerca de '+miubicacion[0]+','+miubicacion[1]);
 		}, 2000);
-	} else { onError();}
+	}
+	function onError() {
+		$('#nogeo').html('');
+		if (navigator.geolocation){
+			$('#nogeo').append("Error: The Geolocation service failed."); 
+			//alert("Error: The Geolocation service failed.");
+			$('#nogeo').css('display', 'inline');
+			$("#nogeo").removeClass().addClass("bounceInLeft");
+			setTimeout(function() {
+				$('#nogeo').removeClass().addClass("bounceOutLeft");
+				//WhereAmI();
+			}, 5000);
+		} else {
+			$('#nogeo').append("Error: Your browser doesn't support geolocation. Are you in Siberia?");
+			//alert("Error: Your browser doesn't support geolocation");
+			$('#nogeo').css('display', 'inline');
+			$("#nogeo").removeClass().addClass("bounceInLeft");
+			setTimeout(function() { 
+				$('#nogeo').removeClass().addClass("bounceOutLeft");
+				//WhereAmI();
+			}, 5000);
+		}
+	}
+	function WhereAmI() {
+		$.getJSON('http://freegeoip.net/json/', function(location) {
+			window.location.replace("/?city="+location.city+"&country="+location.country_name);
+			miubicacion[0]=location.latitude;
+			miubicacion[1]=location.longitude;
+		});
+	}
+	function makeModal() {
 
+	}
+//ROLL ON HOVER
+/*
+	$(function() {
+		$(".roll").css("opacity","0");
+		$(".roll").hover(function () {
+			$(this).stop().animate({
+			opacity: .8
+			}, "slow");
+		},
+		function () {
+			$(this).stop().animate({
+				opacity: 0
+			}, "slow");
+		});
+	});
+*/
+
+$(document).ready(function(e) {
+//	
+	/* Geolocalization HTML5 */
+	if (navigator.geolocation) { navigator.geolocation.getCurrentPosition(showPosition, onError); } else { onError();}
 	/* masonry */
 	$("#contenido").masonry({ itemSelector: 'li' });
 	$("#contenido2").masonry({ itemSelector: 'li' });
 	$("#relacionado").masonry({ itemSelector: 'li' });
 	/* Cuadro de busqueda */
-
 	$('#busqueda').on('click', function() { $('#buscador').toggle().removeClass().addClass("fadeInDown"); });
 	/* Contador de anuncios */
     $(".timer").append('10K');
-
 	/* Menu y derivado */
 	$("#elmenu").on('click', function() { $('#opciones').toggle().removeClass().addClass("fadeInRight");});
 	$("#opciones").on('mouseleave', function(){ setTimeout(function() { $('#opciones').hide(); },3000);});
-
     /* Tooltip */
     $('.fs1').qtip({ style: { classes: 'qtip-light qtip-shadow' } });
     $('header div').qtip({ style: { classes: 'qtip-light qtip-shadow' } });
-
 	/* ET Go Home! */
 	$("#logo").on('click', function(){ window.location = "/"; });
 	/* Login desde Menu */
@@ -83,7 +108,6 @@ $(document).ready(function(e) {
 	$(".showHide-option").rlSmooth('showHide',{ y: 1100 });
 	$(".showUp-option").rlSmooth('showUp',{ y: 1300 });
 	$(".showOut-option").rlSmooth('showOut',{ y: 1500 });
-
 
 //
 });
