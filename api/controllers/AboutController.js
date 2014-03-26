08eccf01330b082be08ac551d7e57f3a1f368272
Daniel Naranjo <dnaranjo@gmail.com>
2014-03-26 12:17:33 -0430

    Contact

--- api/controllers/AboutController.js
+++ api/controllers/AboutController.js
@@ -15,6 +15,8 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+var mailer = require("mailer");
+
 module.exports = {
     
   privacy: function (req, res) {
@@ -28,6 +30,48 @@ module.exports = {
   },
   partner: function (req, res) {
     return res.view();
+  },
+  send: function(req, res, next) {
+
+    var params = req.params.all(),
+      username = "daniel@loultimoenlaweb.com",
+      password = "NNnsEtXx1OCptBjBu2RBtg",
+      name = params.name,
+      email = params.email,
+      message = params.message,
+      now = new Date(),
+      lang = params.lang,
+      country = params.city + params.country,
+      dip = params.dip,
+      bodyMessage = "";
+
+    bodyMessage += 'Contact request through findby.co \n';
+    bodyMessage += '----------------------------------\n';
+    bodyMessage += 'Name: '+ name + '\n';
+    bodyMessage += 'Email: '+ email + '\n';
+    bodyMessage += 'Comments: '+ message + '\n\n\n';
+    bodyMessage += 'Sent time: '+ now + '\n';
+    bodyMessage += '----------------------------------\n';
+    bodyMessage += 'IP: '+ dip + '\n';
+    bodyMessage += 'Language: '+ lang + '\n';
+    bodyMessage += 'Location: '+ country + '\n';
+    bodyMessage += '----------------------------------\n';
+
+    mailer.send({
+      host: "smtp.mandrillapp.com",
+      port: 587,
+      domain: "findby.co",
+      to: "dnaranjo@gmail.com",
+      from: "daniel@loultimoenlaweb.com",
+      subject: "[FindBY] Contact request. Sent "+ now,
+      body: bodyMessage,
+      authentication: "password",
+      username: username,
+      password: password
+    }, function(err, result){
+      if ( err ) return next(err);
+      else return res.redirect('/');
+    });
   }
   
 };
