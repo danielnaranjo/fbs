b59821b9acd38560c0db4838d2d7555b5a0e7d38
Daniel Naranjo <dnaranjo@gmail.com>
2014-06-11 16:48:46 -0430

    Limit 100

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -58,7 +58,8 @@ module.exports = {
         where: where || undefined
       };
       // Find users according with filters
-      Post.find(filters).done(function foundUsers(err, posts){
+      //Post.find(filters).done(function foundUsers(err, posts){
+      Post.find(filters).limit(100).done(function foundUsers(err, posts){
         if ( err ) return next(err);
         // Response JSON if needed.
         if (req.wantsJSON) {
