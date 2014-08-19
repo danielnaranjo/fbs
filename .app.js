4273e08b65903e3a5352d569887e5ebbaf03024f
Daniel Naranjo <dnaranjo@gmail.com>
2014-08-19 08:55:58 -0430

    Disable socket.io in Sails.js
    https://gist.github.com/mikermcneil/8911041

--- .app.js
+++ .app.js
@@ -4,16 +4,27 @@
 // New relic 
 var newrelic = require('newrelic');
 
-// Take from:
-// https://github.com/sgress454/angular-on-sails/blob/master/app.js
-var sails;
+// Take from: https://github.com/sgress454/angular-on-sails/blob/master/app.js
+
+// Disable socket.is
+// https://gist.github.com/mikermcneil/8911041
+var sails, config = {
+  hooks: {
+    sockets: false,
+    pubsub: false
+  }
+};
+
 try {
 	sails = require('sails');
 }
 catch (e) {
-	console.error('Error: Somethings is wrong..');
-	sails.log.verbose('Error: Somethings is wrong..');
+	console.error('Error: Lift is broken! ');
+	sails.log.verbose('Error:  Lift is broken! ');
 	return;
 }
 // Start server
-sails.lift();
+// sails.lift();
+
+// Comes from deactive socket.io solutions
+sails.lift(config);
